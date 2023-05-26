3cfb35c193eb656f4efa9c09bc0ed10c62802475
Awais <125856459+Alihen-code@users.noreply.github.com>
2023-05-26 20:42:58 +0500

    insertion and extraction operators

---
+++ testom.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+	int main()
+	{ 
+	int num1, num2;
+	cout<<"Enter the value of num1: \n";       //'<<' stands for Insertion Operator
+	cin>>num1;
+	cout<<"Enter the value of num2: \n";        // '>>' stands for Extraction Operator 
+	cin>>num2;
+	
+	cout<<"The sum is "<<num1+num2;
+	return 0;
+	}
